2927737d3a5d843b31fc76ac1a3af39ea08e526f
yifattih <yifattih@proton.me>
2025-02-13 06:12:21 +0000

    refactor(service): add minor changes to comply with code quality and format

--- service/src/main.py
+++ service/src/main.py
@@ -1,13 +1,13 @@
 from datetime import datetime, timezone
+
 from fastapi import FastAPI, HTTPException, Response, status
 from prometheus_fastapi_instrumentator import Instrumentator
 
 from .models.model import RMRModel
-from .schemas import InputData, OutputData, HealthStatus
-
+from .schemas import HealthStatus, InputData, OutputData
 
 rmr_model = RMRModel()
-start_time_utc = datetime.now(timezone.utc) # Store the time the api starts
+start_time_utc = datetime.now(timezone.utc)  # Store the time the api starts
 
 app = FastAPI(
     title="Resting Metabolic Rate (RMR) API",
@@ -18,6 +18,7 @@ app = FastAPI(
     version="1.0.0",
 )
 
+
 @app.get("/", tags=["Root"])
 async def root():
     return {
@@ -25,6 +26,7 @@ async def root():
             "Welcome to the RMR API. Use /docs for the API" + " documentation."
         )
     }
+
 
 @app.post("/rmr/", response_model=OutputData)
 async def calculate_rmr(input_data: InputData):
@@ -45,7 +47,11 @@ async def health_check(response: Response):
     current_time_utc = datetime.now(timezone.utc)
     uptime_utc = current_time_utc - start_time_utc
     response.status_code = status.HTTP_200_OK
-    return {"status": "healty", "uptime_utc": str(uptime_utc), "timestamp": str(current_time_utc.isoformat())}
+    return {
+        "status": "healty",
+        "uptime_utc": str(uptime_utc),
+        "timestamp": str(current_time_utc.isoformat()),
+    }
 
 
 # Instrumentation to expose metrics endpoint for Prometheus
