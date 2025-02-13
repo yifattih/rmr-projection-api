3cff40d8e9164bd5ea4c55bd8aef24e47fb6915a
yifattih <yifattih@proton.me>
2025-02-13 06:06:13 +0000

    feat(service/main): add health endpoint allow for api status check

--- service/src/main.py
+++ service/src/main.py
@@ -1,8 +1,13 @@
-from fastapi import FastAPI, HTTPException
+from datetime import datetime, timezone
+from fastapi import FastAPI, HTTPException, Response, status
 from prometheus_fastapi_instrumentator import Instrumentator
 
 from .models.model import RMRModel
-from .schemas import InputData, RMROutput
+from .schemas import InputData, OutputData, HealthStatus
+
+
+rmr_model = RMRModel()
+start_time_utc = datetime.now(timezone.utc) # Store the time the api starts
 
 app = FastAPI(
     title="Resting Metabolic Rate (RMR) API",
@@ -13,9 +18,6 @@ app = FastAPI(
     version="1.0.0",
 )
 
-rmr_model = RMRModel()
-
-
 @app.get("/", tags=["Root"])
 async def root():
     return {
@@ -24,8 +26,7 @@ async def root():
         )
     }
 
-
-@app.post("/rmr/", response_model=RMROutput)
+@app.post("/rmr/", response_model=OutputData)
 async def calculate_rmr(input_data: InputData):
     """
     Endpoint to calculate RMR over a time projection using Mifflin-St. Jeor
@@ -37,6 +38,14 @@ async def calculate_rmr(input_data: InputData):
         raise HTTPException(status_code=400, detail=result["error"])
 
     return {"input": result["input"], "output": result["output"]}
+
+
+@app.get("/health", response_model=HealthStatus)
+async def health_check(response: Response):
+    current_time_utc = datetime.now(timezone.utc)
+    uptime_utc = current_time_utc - start_time_utc
+    response.status_code = status.HTTP_200_OK
+    return {"status": "healty", "uptime_utc": str(uptime_utc), "timestamp": str(current_time_utc.isoformat())}
 
 
 # Instrumentation to expose metrics endpoint for Prometheus
