d6de312d4dec1d62bebd72c887ca567ff7740572
yifattih <yifattih@proton.me>
2025-02-24 19:32:23 +0000

    fix(service): update otel package init file to include logs module

--- service/otel/__init__.py
+++ service/otel/__init__.py
@@ -1,21 +1,34 @@
-from . import otel_metrics
+from . import otel_metrics, otel_logs
 from opentelemetry.instrumentation.logging import LoggingInstrumentor
 
 meter = otel_metrics.meter
+logger = otel_logs.logger
 
 
 try:
     from opentelemetry.instrumentation.fastapi import FastAPIInstrumentor # type: ignore
     instrumentator = FastAPIInstrumentor()
-    log_message = "FastAPI instrumentator initialized"
+    instrumentator_name = "FastAPI"
 except:
     from opentelemetry.instrumentation.flask import FlaskInstrumentor # type: ignore
     instrumentator = FlaskInstrumentor()
-    log_message = "Flask instrumentator initialized"
-
+    instrumentator_name = "Flask"
+finally:
+    logger.info(f"{instrumentator_name} instrumentator found and initialized")
 
 def setup_telemetry(app) -> None:
     """Sets up OpenTelemetry instrumentation for FastAPI."""
+    logger.info("Starting OpenTelemetry configuration setup")
 
-    # Instrument FastAPI
+    # Instrument
     instrumentator.instrument_app(app)
+    logger.info(f"{instrumentator_name} instrumentator created")
+
+    # Instrument Python standard logging
+    LoggingInstrumentor().instrument(
+        set_logging_format=True,
+        logging_format="%(asctime)s %(levelname)s %(name)s %(filename)s:%(lineno)d %(otelTraceID)s %(otelSpanID)s %(message)s",
+    )
+    logger.info("Logging instrumentator initialized")
+
+    logger.info("OpenTelemetry configuration done")
